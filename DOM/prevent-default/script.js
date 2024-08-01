1e3d5fd927221e855ad037efa1e94f985cda65ec
Putri Mila <putrimiila.fx@gmail.com>
2024-08-01 08:11:55 +0800

    add prevent default

---
+++ DOM/prevent-default/script.js
@@ -0,0 +1,32 @@
+// Event handling
+// const close = document.querySelector('.close');
+// const card = document.querySelector('.card');
+
+// close.addEventListener('click', function() {
+//     card.style.display = 'none'; // Tutup kartu saat tombol close diklikiner.style.display = 'none'; // Tutup kartu saat tombol close diklikiner.style.display = 'none'; // Tutup kartu saat tombol close diklik
+// });
+
+
+// DOM Traversal
+const close = document.querySelectorAll('.close');
+
+// for( let i = 0; i < close.length; i++ ) {
+//     close[i].addEventListener('click', function(e) {
+//         // close[i].parentElement.style.display = 'none';
+//         e.target.parentElement.style.display = 'none';
+//     });
+// }
+
+// Prevent Default
+close.forEach(function(el) {
+    el.addEventListener('click', function(e) {
+        e.target.parentElement.style.display = 'none';
+        e.preventDefault(); // Mencegah link yang di-click langsung melakukan navigasi ke alamat yang dituju.
+    });
+});
+
+
+
+
+// const nama = document.querySelector('.nama');
+// console.log(nama.nextElementSibling.nextElementSibling);
