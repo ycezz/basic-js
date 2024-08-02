52f773d98bd651d978786eb8463d73fc4f11435c
Putri Mila <putrimiila.fx@gmail.com>
2024-08-02 10:02:04 +0800

    ok

--- DOM/image-gallery/main.js
+++ DOM/image-gallery/main.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
 
 container.addEventListener('click', function(e) {
 
@@ -7,18 +8,17 @@ container.addEventListener('click', function(e) {
   if( e.target.className == 'thumb' ) {
     jumbo.src = e.target.src;
     jumbo.classList.add('fade');
+    setTimeout(function() {
+      jumbo.classList.remove('fade');
+    }, 500);
 
+    thumbs.forEach(function(thumb) {
+     thumb.className = 'thumb';
+    })
+
+    e.target.classList.add('active');
   }
 });
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const jumbo = document.getElementById('jumbo');
-//   const thumbnails = document.querySelectorAll('.thumb');
 
-//   thumbnails.forEach(thumbnail => {
-//       thumbnail.addEventListener('click', function() {
-//           jumbo.src = this.src;
-//       });
-//   });
-// });
